a6e79d70d0377192a6c0b8f94b3798d1dc4c5fdf
Yuri Luis <71914185+yuriluisz@users.noreply.github.com>
2022-11-10 17:15:53 -0400

    Input area fix

--- Tocar Musica no Youtube/YoutubePlayer.py
+++ Tocar Musica no Youtube/YoutubePlayer.py
@@ -12,7 +12,7 @@ janela.title("abrir musica no youtuba")
 texto = Label(janela, text="Escreva o nome da música e clique no botão para abrir-la no youtube.")
 texto.grid(column=0, row=0, padx=10, pady=10)
 
-inputtxt = Text(janela)
+inputtxt = Text(janela, height=1, width=47)
 inputtxt.grid(column=0,row=1,padx=10,pady=10)
 
 botao = Button(janela, text="Abrir Musica no Youtube", command=abrirMusica)
